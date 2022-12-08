5a7ee5ea0ef04af0d0e471870a7182778a9a6b3a
wayne <4006wayne@gmail.com>
2022-12-08 16:58:34 +0800

    fix: 調整base目錄

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/google-sheet-db/",
+  base: "/google-sheet-db/dist/",
   plugins: [vue()]
 })
